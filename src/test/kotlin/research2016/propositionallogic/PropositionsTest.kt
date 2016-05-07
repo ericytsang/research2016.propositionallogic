c98a86c28320207c27ccecea677f11ada9307944
Eric Tsang <Eric Tsang>
2016-05-06 23:18:30 -0700

    added tests to verify the new combine method

--- src/test/kotlin/research2016/propositionallogic/PropositionsTest.kt
+++ src/test/kotlin/research2016/propositionallogic/PropositionsTest.kt
@@ -82,7 +82,7 @@ class PropositionsTest
         )
         println(proposition)
         println(proposition.models)
-        assert(proposition.models.trueSituations == models)
+        assert(proposition.models.trueSituations == models,{"${proposition.models.trueSituations} != $models"})
     }
 
     @Test
@@ -116,7 +116,70 @@ class PropositionsTest
     @Test
     fun modelsOfBigTree()
     {
-        val proposition = Nand(Xor(Xor(Xor(Oif(Iff(Iff(Iff(Iff(Iff(Nand(Xor(Not(Oif(BasicProposition("p"),And(BasicProposition("q"),BasicProposition("r")))),BasicProposition("s")),BasicProposition("t")),BasicProposition("u")),BasicProposition("v")),BasicProposition("w")),BasicProposition("x")),BasicProposition("y")),BasicProposition("z")),BasicProposition("a")),BasicProposition("b")),BasicProposition("c")),BasicProposition("d"))
-        println(proposition.models)
+        val proposition = Xor(Xor(Xor(Oif(Iff(Iff(Iff(Iff(Iff(Iff(Iff(Iff(Nand(Xor(Not(Oif(BasicProposition("p"),And(BasicProposition("q"),BasicProposition("r")))),BasicProposition("s")),BasicProposition("t")),BasicProposition("u")),BasicProposition("v")),BasicProposition("w")),BasicProposition("x")),BasicProposition("y")),BasicProposition("z")),BasicProposition("a")),BasicProposition("b")),BasicProposition("c")),BasicProposition("d")),BasicProposition("e")),BasicProposition("f"))
+        proposition.models
+    }
+
+    @Test
+    fun modelsCrossCheckWithEvaluateTest1()
+    {
+        val proposition = Not(BasicProposition("q"))
+        val models = proposition.models
+        models.trueSituations.forEach {assert(proposition.evaluate(it))}
+        models.falseSituations.forEach {assert(!proposition.evaluate(it))}
+    }
+
+    @Test
+    fun modelsCrossCheckWithEvaluateTest2()
+    {
+        val proposition = And(BasicProposition("q"),BasicProposition("q"))
+        val models = proposition.models
+        models.trueSituations.forEach {assert(proposition.evaluate(it))}
+        models.falseSituations.forEach {assert(!proposition.evaluate(it))}
+    }
+
+    @Test
+    fun modelsCrossCheckWithEvaluateTest3()
+    {
+        val proposition = Nand(BasicProposition("q"),BasicProposition("q"))
+        val models = proposition.models
+        models.trueSituations.forEach {assert(proposition.evaluate(it))}
+        models.falseSituations.forEach {assert(!proposition.evaluate(it))}
+    }
+
+    @Test
+    fun modelsCrossCheckWithEvaluateTest4()
+    {
+        val proposition = Or(BasicProposition("q"),BasicProposition("q"))
+        val models = proposition.models
+        models.trueSituations.forEach {assert(proposition.evaluate(it))}
+        models.falseSituations.forEach {assert(!proposition.evaluate(it))}
+    }
+
+    @Test
+    fun modelsCrossCheckWithEvaluateTest5()
+    {
+        val proposition = Xor(BasicProposition("q"),BasicProposition("q"))
+        val models = proposition.models
+        models.trueSituations.forEach {assert(proposition.evaluate(it))}
+        models.falseSituations.forEach {assert(!proposition.evaluate(it))}
+    }
+
+    @Test
+    fun modelsCrossCheckWithEvaluateTest6()
+    {
+        val proposition = Iff(BasicProposition("q"),BasicProposition("q"))
+        val models = proposition.models
+        models.trueSituations.forEach {assert(proposition.evaluate(it))}
+        models.falseSituations.forEach {assert(!proposition.evaluate(it))}
+    }
+
+    @Test
+    fun modelsCrossCheckWithEvaluateTest7()
+    {
+        val proposition = Oif(BasicProposition("q"),BasicProposition("q"))
+        val models = proposition.models
+        models.trueSituations.forEach {assert(proposition.evaluate(it))}
+        models.falseSituations.forEach {assert(!proposition.evaluate(it))}
     }
 }
