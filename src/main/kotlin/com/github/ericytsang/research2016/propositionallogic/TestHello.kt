53aa4d572d77cfa4a0a1ff5fcd61b932ac0d0ba8
Eric Tsang <surplus.et@gmail.com>
2016-08-05 01:45:32 -0700

    added test kotlin class...... x____x
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

---
+++ src/main/kotlin/com/github/ericytsang/research2016/propositionallogic/TestHello.kt
@@ -0,0 +1,12 @@
+package com.github.ericytsang.research2016.propositionallogic
+
+/**
+ * Created by surpl on 8/5/2016.
+ */
+class TestHello
+{
+    init
+    {
+        System.out.println("TestHello")
+    }
+}
