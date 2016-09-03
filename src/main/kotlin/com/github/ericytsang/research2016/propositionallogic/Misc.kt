04eb4b13048d953f849edcfb48ef5a51844c7476
Eric Tsang <surplus.et@gmail.com>
2016-09-02 23:17:01 -0700

    made some functions internal
    Signed-off-by: Eric Tsang <surplus.et@gmail.com>

--- src/main/kotlin/com/github/ericytsang/research2016/propositionallogic/Misc.kt
+++ src/main/kotlin/com/github/ericytsang/research2016/propositionallogic/Misc.kt
@@ -5,13 +5,13 @@ import java.util.NoSuchElementException
 /**
  * Created by surpl on 9/2/2016.
  */
-fun <T> List<T>.getRandom():T
+internal fun <T> List<T>.getRandom():T
 {
     if (isEmpty()) throw NoSuchElementException("list is empty.")
     return this[(Math.random()*size).toInt()]
 }
 
-fun <T> Array<T>.getRandom():T
+internal fun <T> Array<T>.getRandom():T
 {
     if (isEmpty()) throw NoSuchElementException("list is empty.")
     return this[(Math.random()*size).toInt()]
